f84f6ed41c28f45d5693eb500df8d2bfea0a79bc
Aaron Rodecker <aaron.rodecker@dynepic.com>
2025-02-18 19:57:53 -0800

    e2e whopsy

--- apps/angular-task-e2e/src/test-search.spec.ts
+++ apps/angular-task-e2e/src/test-search.spec.ts
@@ -34,7 +34,7 @@ test('has details page route', async ({ page }) => {
     await page.goto('/');
     const inputSearch = page.locator('.search-container input');
     await inputSearch.scrollIntoViewIfNeeded();
-    await inputSearch.type("glenna reichert");
+    await inputSearch.type("test@user.com");
     const detailsSelector = page.locator('.users-list crx-user-card .card');
     await detailsSelector.click();
 
